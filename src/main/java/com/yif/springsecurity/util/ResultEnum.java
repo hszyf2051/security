503b029d262b60a2fa520f023364bd3cccadd406
yif <151900983@qq.com>
2023-07-11 16:47:11 +0800

    init

---
+++ src/main/java/com/yif/springsecurity/util/ResultEnum.java
@@ -0,0 +1,34 @@
+package com.yif.springsecurity.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author Yif
+ * @description 返回参数枚举类
+ **/
+@Getter
+@AllArgsConstructor
+public enum ResultEnum {
+
+    SUCCESS(200, "成功"),
+
+    ERROR(10001, "错误"),
+
+    UNAUTHENTICATED(401, "请先通过身份认证"),
+    AUTH_FAIL(1400, "认证失败"),
+
+    // token异常
+    TOKEN_PAST(1401, "身份过期，请求重新登录！"),
+    TOKEN_ERROR(1402, "令牌错误"),
+    HEADER_ERROR(1403, "请求头错误"),
+    AUTH_USERNAME_NONE(1405, "用户名不能为空"),
+    AUTH_PASSWORD_NONE(1406, "密码不能为空"),
+
+    MENU_NO(306, "没此权限，请联系管理员！"),
+
+    SYSTEM_ERROR(50000, "系统内部异常");
+
+    private final Integer code;
+    private final String desc;
+}
